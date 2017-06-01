a970d0aec7eefe4626cb1cbc58603c4a45166848
kah5 <kellyannheilman@gmail.com>
2017-06-01 17:29:42 -0500

    commit changes before pull

--- 02_prelim_data_plots.R
+++ 02_prelim_data_plots.R
@@ -2,12 +2,20 @@ library(raster)
 library(plyr)
 library(ggplot2)
 library(reshape2)
-# read in model outputs that were extracted
+# load in the lat lon of all the sites
+load("Data/PalEON_siteInfo_all.RData")
 
-ED2.CO2 <- readRDS(file = "Data/ED_montly_CO2.RDS")
-ED2.GPP <- readRDS(file = "Data/ED_montly_gpp.RDS")
-ED2.gwbi <- readRDS(file = "Data/ED_montly_gwbi.RDS")
-#test<- raster(ED2.CO2)
+# function read in model outputs that were extracted
+ED2.CO2 <- readRDS(file = "Data/ED2/ED2.CO2.rds")
+ED2.GPP <- readRDS(file = "Data/ED2/ED2.GPP.rds")
+ED2.gwbi <- readRDS(file = "Data/ED2/ED2.GWBI.rds")
+
+
+colnames(ED2.CO2) <- paleon$latlon
+
+
+plot(ED2.CO2[,1], ED2.GPP[,1])
+plot(ED2.CO2[,1], ED2.gwbi[,1])
 
 ed.co2<- ED2.CO2$CO2
 Year <- yearno+850
